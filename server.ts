50039ee30b05618728697d1c4f7930b289d37606
Neon <andrerisrodriguez@gmail.com>
2026-02-18 21:45:52 -0400

    Probando despliegue

--- server.ts
+++ server.ts
@@ -186,7 +186,10 @@ app.post('/reservations', (req: Request, res: Response) => {
 
 seedData();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+// app.listen(PORT, () => {
+app.listen(Number(PORT), '0.0.0.0', () => {
     //Agregar servidor en Render y actualizar URL en openapi.yaml
   console.log(`Servidor corriendo en https://restaurantelaterraza-production.up.railway.app/`);
 });
+
